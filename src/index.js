804fa8f7289c4f79028636d3876b55a80aaf81d1
Jader Cano <jader.cano@contractors.accruent.com>
2021-05-03 16:58:38 -0500

    Added BadgesList component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./global.css";
-import BadgeNew from "./pages/BadgeNew";
+import Badges from "./pages/Badges";
 
 const container = document.getElementById("app");
 
 //const element = <h1>Hello, Platzi Badges!</h1>;
 //const element = React.createElement("h1", {}, "Hello, I was created using createElement");
 
-ReactDOM.render(<BadgeNew />, container);
+ReactDOM.render(<Badges />, container);
